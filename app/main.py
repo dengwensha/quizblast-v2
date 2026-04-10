823541fb81e1f1671ed6ed4be7f9be36707e9839
ilkay kinaci <ilkaykinaci@gmail.com>
2026-04-10 12:28:01 +0300

    add frontend صفحات

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "QuizBlast running"}
+    return FileResponse("static/index.html")
+
+@app.get("/host")
+def host():
+    return FileResponse("static/host.html")
 
 @app.get("/health")
 def health():
