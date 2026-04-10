b7714d84399a47bd58f376fdc9f5ca4fcb839121
ilkay kinaci <ilkaykinaci@gmail.com>
2026-04-10 12:52:16 +0300

    add websocket

--- app/main.py
+++ app/main.py
@@ -17,3 +17,19 @@ def host():
 @app.get("/health")
 def health():
     return {"ok": True}
+from fastapi import WebSocket
+
+clients = []
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    clients.append(websocket)
+
+    try:
+        while True:
+            data = await websocket.receive_text()
+            for client in clients:
+                await client.send_text(data)
+    except:
+        clients.remove(websocket)
