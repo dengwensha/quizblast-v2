f046da0eb3f63024ea476fe816a9e7e0f66fd04e
ilkay kinaci <ilkaykinaci@gmail.com>
2026-04-10 12:36:53 +0300

    fix static path

--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
+import os
 
 app = FastAPI()
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 @app.get("/")
 def root():
-    return FileResponse("static/index.html")
+    return FileResponse(os.path.join(BASE_DIR, "static/index.html"))
 
 @app.get("/host")
 def host():
-    return FileResponse("static/host.html")
+    return FileResponse(os.path.join(BASE_DIR, "static/host.html"))
 
 @app.get("/health")
 def health():
